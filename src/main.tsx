8d299eddd13de2a5d59b6775f6d1cc7f0d43c4a9
henhernquist-lab <henhernquist@gmail.com>
2026-07-13 20:10:47 -0400

    Enry Cruise: Set darkMode: 'class' in tailwind config, created theme-provider with next-themes, and wrapped App in BrowserRouter + ThemeProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { ThemeProvider } from "./components/theme-provider.tsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { AppThemeProvider } from './components/theme-provider';
+import App from './App';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(
-  <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-    <App />
-  </ThemeProvider>
+createRoot(document.getElementById('root')!).render(
+	<StrictMode>
+		<BrowserRouter>
+			<AppThemeProvider>
+				<App />
+			</AppThemeProvider>
+		</BrowserRouter>
+	</StrictMode>,
 );
